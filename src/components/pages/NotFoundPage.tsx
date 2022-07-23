0342204988e27b8ebc7cca4b791566227f8321a2
Vasyl Popovych <popovich.w.ppc@gmail.com>
2022-07-23 17:05:29 +0300

    stylized NotFoundPage

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -1,10 +1,21 @@
+import { Box, Container, Typography } from '@mui/material'
 import React from 'react'
 
 type Props = {}
 
-const NotFoundPage = (props: Props) => {
+const NotFoundPage:React.FC = (props: Props) => {
   return (
-    <div>NotFoundPage</div>
+    <Container maxWidth={false} sx={{background: '#5D5D67'}}>
+      <Box
+    height="100vh"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <Typography variant="h1">Page not found</Typography>
+  </Box>
+    </Container>
+    
   )
 }
 
