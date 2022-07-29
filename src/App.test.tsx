2c565c0fa81cdf5e46770cf245e894b36032c3f1
Yegor Kochetkov <kegor.fx@gmail.com>
2022-07-29 13:50:28 +0300

    Add  test for form

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders components", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Working with POST request/i);
   expect(linkElement).toBeInTheDocument();
 });
