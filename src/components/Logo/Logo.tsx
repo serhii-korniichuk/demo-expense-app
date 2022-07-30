baac2cd9f11b580a223eb0a16814b1867b26d541
Yegor Kochetkov <kegor.fx@gmail.com>
2022-07-30 03:23:52 +0300

    Add logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Logo.scss";
+
+export const Logo: React.FC = () => (
+  <div className="logo">
+    <span className="logo__primary-text">
+      InCode
+    </span>
+    <br />
+    <span className="logo__secondary-text">
+      Finance
+    </span>
+  </div>
+);
