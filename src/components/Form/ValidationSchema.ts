10454f6f48d6cd643e3753f101c4e49aac670bb8
Yegor Kochetkov <kegor.fx@gmail.com>
2022-07-30 19:13:23 +0300

    Fix validation schema

--- src/components/Form/ValidationSchema.ts
+++ src/components/Form/ValidationSchema.ts
@@ -1,13 +1,10 @@
 import * as Yup from "yup";
 
-// eslint-disable-next-line no-control-regex
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
-const usernameRegex = /^[a-z0-9_-]{1,24}$/;
 
 export const SignInSchema = Yup.object().shape({
   username: Yup
     .string()
-    .matches(usernameRegex, "Invalid username")
     .min(2, "Too short!")
     .max(60, "Too long!")
     .required("User name is required"),
@@ -15,17 +12,16 @@ export const SignInSchema = Yup.object().shape({
     .string()
     .matches(
       passwordRegex,
-      `Password should be at least 8 characters,
-      must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number.
+      `Must contain uppercase/lowercase letters and numbers.
       Can contain special characters`,
     )
-    .min(8, "Too short!")
+    .min(8, "Password should be at least 8 characters")
     .max(40, "Too long!")
     .required("Password is required"),
 });
 
 export const SignUpSchema = SignInSchema.concat(Yup.object().shape({
-  fullname: Yup
+  displayname: Yup
     .string()
     .min(1, "Too short!")
     .max(40, "Too long!")
