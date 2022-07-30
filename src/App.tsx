59993bae48e644e00852603293191670d231ab0f
Yegor Kochetkov <kegor.fx@gmail.com>
2022-07-30 03:24:47 +0300

    Fix layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
 import { Form } from "./components/Form";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Form />
     </div>
   );
