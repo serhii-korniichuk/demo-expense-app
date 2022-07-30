9c13ef9fed56c7ed78743944d900d25420a69aae
Yegor Kochetkov <kegor.fx@gmail.com>
2022-07-30 03:24:14 +0300

    Add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Logo } from "../Logo";
+import "./Header.scss";
+
+export const Header: React.FC = () => (
+  <header className="header">
+    <Logo />
+  </header>
+);
