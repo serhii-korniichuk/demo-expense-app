695d85aebc2920e063bdfecb29802e09e5e75639
Vasyl Popovych <popovich.w.ppc@gmail.com>
2022-08-01 11:29:49 +0300

    change BrowserRouter to HashRouter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Router from './components/Router';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Router/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
