ae52fa89a11a5ef341f16a5af8f3112e6878dfc0
Dima Syzonenko <sizonenko.d18@gmail.com>
2022-08-26 06:57:53 +0300

    basename changed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Auth'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
