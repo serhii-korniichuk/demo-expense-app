cd5c20617ac5e8535951dcfa85ef83f99a4f5ee3
Vlad Vylotnilov <vladvyljotnikov@gmail.com>
2022-09-16 23:38:43 +0300

    change header title

--- src/components/HeaderAuth/HeaderAuth.jsx
+++ src/components/HeaderAuth/HeaderAuth.jsx
@@ -5,7 +5,7 @@ import styles from "./HeaderAuth.module.scss";
 export const HeaderAuth = () => {
     return (
         <header className={styles.header}>
-            <h3>InCode</h3>
+            <h3>Code</h3>
             <h5>Finance</h5>
         </header>
     );
