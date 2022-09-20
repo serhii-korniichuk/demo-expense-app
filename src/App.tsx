4a1afc3050f71966922f56aa8e59a37332c5e5b7
denis-khristyuk <monsieur,apala@gmail.com>
2022-09-20 17:53:12 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/main.scss';
+import './styles/App.scss';
+import { SignIn } from './components/SignIn/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SignIn = () => {
+    return (
+        <div className="signIn">
+            <h1 className="signIn__title">InCode</h1>
+        </div>
+    )
+}
