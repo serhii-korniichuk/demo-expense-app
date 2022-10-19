e851d1f7e04e2374225695a6599a0212d943fef2
Anastasiia Khmelovska <hmelanast1@gmail.com>
2022-10-19 14:54:21 +0300

    Solution)

---
+++ src/components/fetch.ts
@@ -0,0 +1,41 @@
+const BASE_URL = 'https://incode-backend-dev.herokuapp.com';
+
+function wait(delay: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, delay);
+  });
+}
+
+type RequestMethod = 'GET' | 'POST';
+
+function request<T>(
+  url: string,
+  method: RequestMethod = 'GET',
+  data: any = null,
+): Promise<T> {
+  const options: RequestInit = { method };
+
+  if (data) {
+    options.body = JSON.stringify(data);
+    options.headers = {
+      'Content-Type': 'application/json; charset=UTF-8',
+    };
+  }
+
+  return wait(300)
+    .then(() => fetch(BASE_URL + url, options))
+    .then(response => {
+      // eslint-disable-next-line no-console
+      console.log(response);
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      return response.json();
+    });
+}
+
+export const client = {
+  get: <T>(url: string) => request<T>(url),
+  post: <T>(url: string, data: any) => request<T>(url, 'POST', data),
+};
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import { App } from './App';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
