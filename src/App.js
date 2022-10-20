b5cff3e4372a83026eb301c1f117df705562384e
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-10-21 01:13:11 +0300

    route rules added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/Home/Home";
+import { useSelector } from "react-redux";
+import authSelectors from "./redux/authSelectors";
 
 function App() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
   return (
     <Routes>
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/home" element={<Home />} />
+      <Route
+        path="/auth"
+        element={!isLoggedIn ? <Auth /> : <Navigate to="/home" />}
+      />
+      <Route
+        path="/home"
+        element={isLoggedIn ? <Home /> : <Navigate to="/auth" />}
+      />
       <Route path="*" element={<Navigate to="/auth" />} />
     </Routes>
   );
