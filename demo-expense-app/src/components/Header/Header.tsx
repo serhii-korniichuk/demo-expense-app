6e22378cdfbe39fdaf20d640147a7ebe00eed481
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-11-06 21:29:19 +0200

    functionality added, design finished commit
    rest function finished, design finished
    
    form validation, refactoring required

---
+++ demo-expense-app/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss";
+export const Header = () => {
+  return (
+    <header className="header">
+      <span className="logo">InCode</span>
+      <span className="department">Finance</span>
+    </header>
+  );
+};
--- demo-expense-app/src/components/pages/Auth/Auth.tsx
+++ demo-expense-app/src/components/pages/Auth/Auth.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { SignIn } from "./SignIn";
 import { SignUp } from "./SignUp";
+import { Header } from "components/Header";
 import AuthDiv from "./generic/AuthDiv";
 import "./Auth.scss";
 
 export const Auth = () => {
   return (
-    <div className="content">
+    <div className="authPage">
+      <Header></Header>
       <AuthDiv></AuthDiv>
     </div>
   );
